170eda997f91854b953593369b8f17fcd2f6d933
Ziad Nagah <ziadnagah14@gmail.com>
2022-12-25 21:42:23 +0200

    adding two files

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 25 21:35:22 2022
+
+@author: Ziad
+"""
+
---
+++ ziad.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 25 21:35:33 2022
+
+@author: Ziad
+"""
+
